2049018206826f276f10e2a8ac34cc3e4e6a0ad9
张锦华 <colin.zhang@zsgcm.com>
2021-09-11 15:18:24 +0800

    cell turn when cell is dead and near count equals two then cell is dead

--- src/test/java/org/example/CellTest.java
+++ src/test/java/org/example/CellTest.java
@@ -96,4 +96,13 @@ public class CellTest {
 
         Assert.assertTrue(newCell.isDead());
     }
+
+    @Test
+    public void cell_turn_when_cell_is_dead_and_near_count_equals_two_then_cell_is_dead() {
+        Cell originCell = new Cell(0,0, false);
+
+        Cell newCell = originCell.turn(2);
+
+        Assert.assertTrue(newCell.isDead());
+    }
 }
