791a55b9fc819fbbae04f7425d0e7fb372ac0edb
张锦华 <colin.zhang@zsgcm.com>
2021-09-11 16:07:03 +0800

    equals two case complete

--- src/test/java/org/example/BoardTest.java
+++ src/test/java/org/example/BoardTest.java
@@ -82,4 +82,80 @@ public class BoardTest {
                 {"O", "O", "O"}
         });
     }
+
+    @Test
+    public void equals_two_case_1() {
+        Board board = new Board(new String[][]{
+                {"O", "X", "O"},
+                {"O", "O", "X"},
+                {"O", "O", "O"}
+        });
+
+        board.turn();
+
+        String[][] result = board.currentStatus();
+
+        Assert.assertArrayEquals(result, new String[][]{
+                {"O", "O", "O"},
+                {"O", "O", "O"},
+                {"O", "O", "O"}
+        });
+    }
+
+    @Test
+    public void equals_two_case_2() {
+        Board board = new Board(new String[][]{
+                {"O", "X", "O"},
+                {"O", "X", "X"},
+                {"O", "O", "O"}
+        });
+
+        board.turn();
+
+        String[][] result = board.currentStatus();
+
+        Assert.assertArrayEquals(result, new String[][]{
+                {"O", "X", "X"},
+                {"O", "X", "X"},
+                {"O", "O", "O"}
+        });
+    }
+
+    @Test
+    public void equals_two_case_3() {
+        Board board = new Board(new String[][]{
+                {"X", "O", "O"},
+                {"O", "O", "X"},
+                {"O", "O", "O"}
+        });
+
+        board.turn();
+
+        String[][] result = board.currentStatus();
+
+        Assert.assertArrayEquals(result, new String[][]{
+                {"O", "O", "O"},
+                {"O", "O", "O"},
+                {"O", "O", "O"}
+        });
+    }
+
+    @Test
+    public void equals_two_case_4() {
+        Board board = new Board(new String[][]{
+                {"X", "O", "O"},
+                {"O", "X", "X"},
+                {"O", "O", "O"}
+        });
+
+        board.turn();
+
+        String[][] result = board.currentStatus();
+
+        Assert.assertArrayEquals(result, new String[][]{
+                {"O", "X", "O"},
+                {"O", "X", "O"},
+                {"O", "O", "O"}
+        });
+    }
 }
